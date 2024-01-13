e84f337d36ff2f49b69942a93ee92edd168b4197
El1zabethS <bestlemon555@gmail.com>
2024-01-13 11:26:33 -0700

    Updated Casing for drive Susbsytem

--- src/main/java/frc/robot/Subsystems/Drive/moduleIO.java
+++ src/main/java/frc/robot/Subsystems/Drive/moduleIO.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Subsystems.Drive;
+package frc.robot.Subsystems.drive;
 
 /** Add your docs here. */
-public class moduleIO {}
+public class ModuleIO {}
