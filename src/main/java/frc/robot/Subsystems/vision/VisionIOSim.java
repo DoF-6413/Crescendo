931699c6dc5b7a96c8c8cb8a298572b9ee50cf20
alt09 <andreslaratrevino@gmail.com>
2024-01-14 15:45:55 -0700

    create and finish the vision IOSim (untested)

---
+++ src/main/java/frc/robot/Subsystems/vision/VisionIOSim.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems.vision;
+
+import org.photonvision.PhotonCamera;
+
+import edu.wpi.first.apriltag.AprilTagFields;
+
+/** Add your docs here. */
+public class VisionIOSim extends VisionIO {
+
+    private static PhotonCamera camera = new PhotonCamera("Arducam_IMX298_Camera_2_Top"); // TODO: update name
+    private final AprilTagFields apriltags = AprilTagFields.k2024Crescendo;
+
+    public void updateInputs(VisionIOInputs inputs) {
+        inputs.PhotonPipelineResult = camera.getLatestResult();
+        inputs.HasTargets = camera.getLatestResult().hasTargets();
+        if (inputs.HasTargets == true) {
+            inputs.Target = camera.getLatestResult().getBestTarget();
+            inputs.BestFiducialID = camera.getLatestResult().getBestTarget().getFiducialId();
+            inputs.BestCamToTarget = camera.getLatestResult().getBestTarget().getBestCameraToTarget();
+            inputs.TargetX = camera.getLatestResult().getBestTarget().getBestCameraToTarget().getX();
+            inputs.TargetY = camera.getLatestResult().getBestTarget().getBestCameraToTarget().getY();
+            inputs.TargetZ = camera.getLatestResult().getBestTarget().getBestCameraToTarget().getZ();
+            inputs.TargetYaw = camera.getLatestResult().getBestTarget().getYaw();
+            inputs.TargetPitch = camera.getLatestResult().getBestTarget().getPitch();
+            inputs.TargetArea = camera.getLatestResult().getBestTarget().getArea();
+        }
+    }
+}
