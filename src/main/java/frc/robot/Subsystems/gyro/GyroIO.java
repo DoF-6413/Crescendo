6f9f17866baa8108e6eafc9a537cd97cf3582a78
El1zabethS <BestLemon555@gmail.com>
2024-01-14 13:31:30 -0700

    GyroIO/Inputs Filled out

--- src/main/java/frc/robot/Subsystems/gyro/GyroIO.java
+++ src/main/java/frc/robot/Subsystems/gyro/GyroIO.java
@@ -11,8 +11,21 @@ import org.littletonrobotics.junction.AutoLog;
 public interface GyroIO {
 
   @AutoLog
-  public static class GyroIOInputs {}
+  public static class GyroIOInputs {
+    public boolean connected = false;
+    public double rollPositionRad = 0.0;
+    public double pitchPositionRad = 0.0;
+    public double yawPositionRad = 0.0;
+    public double anglePositionRad = 0.0;
+    public double rate = 0.0;
+    public double rollVelocityRadPerSec = 0.0;
+    public double pitchVelocityRadPerSec = 0.0;
+    public double yawVelocityRadPerSec = 0.0;
+    public double heading = 0.0;
+  }
 
   /** Updates the set of loggable inputs. */
   public default void updateInputs(GyroIOInputs inputs) {}
+
+  public default void updateHeading(){}
 }
