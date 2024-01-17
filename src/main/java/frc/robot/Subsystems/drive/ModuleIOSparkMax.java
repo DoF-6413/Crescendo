0d146137a757580e0099d4a05a341fb759df4f55
El1zabethS <BestLemon555@gmail.com>
2024-01-16 18:32:44 -0700

    Added the CANID's for the real version of the L2 Module Drivetrain

--- src/main/java/frc/robot/Subsystems/drive/ModuleIOSparkMax.java
+++ src/main/java/frc/robot/Subsystems/drive/ModuleIOSparkMax.java
@@ -12,6 +12,9 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.util.Units;
 import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.DriveConstants.ABSOLUTE_ENCODER;
+import frc.robot.Constants.DriveConstants.DRIVE_MOTOR;
+import frc.robot.Constants.DriveConstants.TURN_MOTOR;
 
 /** Runs an Individual Real Module with all Motors as Neos */
 public class ModuleIOSparkMax implements ModuleIO {
@@ -20,24 +23,41 @@ public class ModuleIOSparkMax implements ModuleIO {
 
   private final RelativeEncoder driveEncoder;
   private final RelativeEncoder turnRelativeEncoder;
-  private final CANcoder turnAbsoluteEncoder; 
+  private final CANcoder turnAbsoluteEncoder;
 
-  private final double turnAfterEncoderReduction = 150.0 / 7.0; //TODO: fact check
+  private final double turnAfterEncoderReduction = 150.0 / 7.0; // TODO: fact check
   private final boolean isTurnMotorInverted = true;
   private final int nancy;
-
-  private final Pigeon2 gyro;
 
   public ModuleIOSparkMax(int index) {
     System.out.println("[Init] Creating ModuleIOSparkMax");
     this.nancy = index;
 
-    switch(index) {
+    switch (index) {
       case 0:
-        driveSparkMax = new CANSparkMax(DriveMotor.frontLeft.CAN_ID, null); //update constants
+        driveSparkMax = new CANSparkMax(DRIVE_MOTOR.FRONT_RIGHT.CAN_ID, MotorType.kBrushless);
+        turnSparkMax = new CANSparkMax(TURN_MOTOR.FRONT_RIGHT.CAN_ID, MotorType.kBrushless);
+        turnAbsoluteEncoder = new CANcoder(ABSOLUTE_ENCODER.FRONT_RIGHT.ENCODER_ID);
+        break;
+      case 1:
+        driveSparkMax = new CANSparkMax(DRIVE_MOTOR.FRONT_LEFT.CAN_ID, MotorType.kBrushless);
+        turnSparkMax = new CANSparkMax(TURN_MOTOR.FRONT_LEFT.CAN_ID, MotorType.kBrushless);
+        turnAbsoluteEncoder = new CANcoder(ABSOLUTE_ENCODER.FRONT_LEFT.ENCODER_ID);
+        break;
+      case 2:
+        driveSparkMax = new CANSparkMax(DRIVE_MOTOR.FRONT_LEFT.CAN_ID, MotorType.kBrushless);
+        turnSparkMax = new CANSparkMax(TURN_MOTOR.FRONT_LEFT.CAN_ID, MotorType.kBrushless);
+        turnAbsoluteEncoder = new CANcoder(ABSOLUTE_ENCODER.FRONT_LEFT.ENCODER_ID);
+        break;
+      case 3:
+        driveSparkMax = new CANSparkMax(DRIVE_MOTOR.FRONT_LEFT.CAN_ID, MotorType.kBrushless);
+        turnSparkMax = new CANSparkMax(TURN_MOTOR.FRONT_LEFT.CAN_ID, MotorType.kBrushless);
+        turnAbsoluteEncoder = new CANcoder(ABSOLUTE_ENCODER.FRONT_LEFT.ENCODER_ID);
+        break;
+      default:
+        throw new RuntimeException("Invalid module index for ModuleIOSparkMax");
     }
 
-    gyro = new Pigeon2(0); //TODO: Update CANIDs
   }
 
   @Override
