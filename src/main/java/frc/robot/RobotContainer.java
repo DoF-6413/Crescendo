d9cb0302cf01a4e624bbf637d82c304a824173a5
Liz <92767215+El1zabethS@users.noreply.github.com>
2024-01-19 19:01:19 -0700

    Drive Sim (#23)
    * Added Simulation, Needs Testing
    
    * Ready for Testing, Needs Controller Bindings
    
    * added controller
    
    * updated beta #
    
    * Updated Comments
    
    * Updated for PR

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,9 +16,17 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.RobotStateConstants;
+import frc.robot.Subsystems.drive.Drive;
+import frc.robot.Subsystems.drive.ModuleIO;
+import frc.robot.Subsystems.drive.ModuleIOSimNeo;
+import frc.robot.Subsystems.drive.ModuleIOSparkMax;
+import frc.robot.Subsystems.gyro.Gyro;
+import frc.robot.Subsystems.gyro.GyroIO;
+import frc.robot.Subsystems.gyro.GyroIONavX;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -28,9 +36,10 @@ import frc.robot.Constants.RobotStateConstants;
  */
 public class RobotContainer {
   // Subsystems
-
+  private final Gyro m_gyroSubsystem;
+  private final Drive m_driveSubsystem;
   // Controllers
-  private final CommandXboxController controller =
+  private final CommandXboxController driverController =
       new CommandXboxController(OperatorConstants.DRIVE_CONTROLLER);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -38,14 +47,38 @@ public class RobotContainer {
     switch (RobotStateConstants.getMode()) {
       case REAL:
         // Real robot, instantiate hardware IO implementations
+        m_gyroSubsystem = new Gyro(new GyroIONavX());
+        m_driveSubsystem =
+            new Drive(
+                new ModuleIOSparkMax(),
+                new ModuleIOSparkMax(),
+                new ModuleIOSparkMax(),
+                new ModuleIOSparkMax(),
+                m_gyroSubsystem);
         break;
 
       case SIM:
         // Sim robot, instantiate physics sim IO implementations
+        m_gyroSubsystem = new Gyro(new GyroIO() {});
+        m_driveSubsystem =
+            new Drive(
+                new ModuleIOSimNeo(),
+                new ModuleIOSimNeo(),
+                new ModuleIOSimNeo(),
+                new ModuleIOSimNeo(),
+                m_gyroSubsystem);
         break;
 
       default:
         // Replayed robot, disable IO implementations
+        m_gyroSubsystem = new Gyro(new GyroIO() {});
+        m_driveSubsystem =
+            new Drive(
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                m_gyroSubsystem);
         break;
     }
 
@@ -59,7 +92,18 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+
+    // A default command always runs unless another command is called
+    m_driveSubsystem.setDefaultCommand(
+        new InstantCommand(
+            () ->
+                m_driveSubsystem.setRaw(
+                    driverController.getLeftX(),
+                    driverController.getLeftY(),
+                    driverController.getRightX()),
+            m_driveSubsystem));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
