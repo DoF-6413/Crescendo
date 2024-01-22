605f2157a83a0476a21181f4b8cc2c5e343a21d4
hh3len <89624311+hh3len@users.noreply.github.com>
2024-01-22 01:01:04 -0700

    initial commit outline for mechanisms2d

---
+++ src/main/java/frc/robot/Subsystems/mechanism2d/mechanisms2d.java
@@ -0,0 +1,20 @@
+package frc.robot.Subsystems.mechanism2d;
+
+import edu.wpi.first.wpilibj.smartdashboard.*;
+import frc.robot.Subsystems.Constants.*;
+
+
+public class mechanisms2d extends SubsystemBase {
+    // Mechanism2d has plenty of simulations to choose these are just ones we'll probably use
+    // private final Mechanism 2d m_swerveModules;
+    // private final MechanismLigament2d m_elevator;
+    // private final Elevator m_elevator; 
+    // private final Wrist m_wrist;
+
+    public mechanisms2d() {
+    }
+
+    @Override
+    public void periodic() {
+    }
+}
