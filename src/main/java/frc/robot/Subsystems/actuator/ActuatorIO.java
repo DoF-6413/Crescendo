cfbd61ca56fe1a9e1793f9b9e7cb3b5a2cb68394
nancyesquivel10 <nancyesquivel505@gmail.com>
2024-01-31 19:00:23 -0700

    incorperated PID into the OTB intake subsystem

--- src/main/java/frc/robot/Subsystems/actuator/ActuatorIO.java
+++ src/main/java/frc/robot/Subsystems/actuator/ActuatorIO.java
@@ -3,7 +3,6 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.Subsystems.actuator;
-
 import org.littletonrobotics.junction.AutoLog;
 
 public interface ActuatorIO {
