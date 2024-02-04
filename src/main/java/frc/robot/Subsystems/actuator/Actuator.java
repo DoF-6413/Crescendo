9e975c2619960d05bef5d05f785bd01ef1adaa10
nancyesquivel10 <nancyesquivel505@gmail.com>
2024-02-04 15:22:10 -0700

    finished fixing actuator PID command and added smartdashboard methods where you can put and collect the values

--- src/main/java/frc/robot/Subsystems/actuator/Actuator.java
+++ src/main/java/frc/robot/Subsystems/actuator/Actuator.java
@@ -16,7 +16,7 @@ public class Actuator extends SubsystemBase {
 
   public static ActuatorIOInputsAutoLogged actuatorInputs;
   public static PIDController actuatorPID;
-  private double actuatorSetpoint;
+  private double actuatorSetpoint = 0.0;
 
   public Actuator(ActuatorIO io) {
     System.out.println("[init] creating Actuator");
@@ -26,26 +26,46 @@ public class Actuator extends SubsystemBase {
             ActuatorConstants.ACTUATOR_KP,
             ActuatorConstants.ACTUATOR_KI,
             ActuatorConstants.ACTUATOR_KD);
+
+    actuatorPID.setSetpoint(actuatorSetpoint);
+
+    // This sets constraints on the rot of the actuator (takes shortest path between setpoints so it
+    // doesn't go all the way around)
+    actuatorPID.enableContinuousInput(
+        ActuatorConstants.ACTUATOR_MIN_SETPOINT, ActuatorConstants.ACTUATOR_MIN_SETPOINT);
+
     actuatorPID.setTolerance(actuatorSetpoint * ActuatorConstants.ACTUATOR_TOLERANCE);
+
+    // Creates adjustible PID values and setpoints on smartDashboard (for easier testing)
+    SmartDashboard.putNumber("actuatorkp", 0.0);
+    SmartDashboard.putNumber("actuatorki", 0.0);
+    SmartDashboard.putNumber("actuatorkd", 0.0);
+
+    SmartDashboard.putNumber("actuatorSetpoint", 0.0);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-    actuatorIO.updateInputs(actuatorInputs); // update the inputs
+    actuatorIO.updateInputs(actuatorInputs); // updates the inputs
     Logger.processInputs("Actuator", actuatorInputs); // logg the inputs
-    actuatorIO.setActuatorSpeed(setpointRPM);
-    actuatorIO.setActuatorVoltage(actuatorPID.calculate(getActuatorPositionRad(), ));   //TODO: fix this to match actuator position instead of RPM 
+
+    // This updates the PID values that are put in the SmartDashboard
     if (ActuatorConstants.ACTUATOR_KP != SmartDashboard.getNumber("actuatorkp", 0)
         || ActuatorConstants.ACTUATOR_KI != SmartDashboard.getNumber("actuatorki", 0)
         || ActuatorConstants.ACTUATOR_KD != SmartDashboard.getNumber("actuatorkd", 0)) {
       updatePIDController();
     }
 
-    updateSetpoint(SmartDashboard.getNumber("setpoint", 0));
+    // This updates the setpoint if changed in the SmartDashboard
+    if (actuatorSetpoint != SmartDashboard.getNumber("actuatorSetpoint", 0.0)) {
+      updateSetpoint();
+    }
 
+    // For testing- making sure you are getting the values you are giving it
     SmartDashboard.putNumber("Actuator setpoint", actuatorSetpoint);
     SmartDashboard.putNumber("ActuatorError", actuatorSetpoint - getActuatorPosition());
+
+    actuatorIO.setActuatorPosition(actuatorPID.calculate(getActuatorPositionRad()));
   }
 
   private void updatePIDController() {
@@ -59,7 +79,9 @@ public class Actuator extends SubsystemBase {
             ActuatorConstants.ACTUATOR_KD);
   }
 
-  public double getActuatorPosition() { // return the position in meters
+  public double
+      getActuatorPosition() { // return the position in meters (just for human understanding bc we
+    // use rad)
     return actuatorInputs.turnPositionRad * 2 * Math.PI;
   }
 
@@ -67,7 +89,7 @@ public class Actuator extends SubsystemBase {
     actuatorIO.setActuatorSpeed(voltage);
   }
 
-  public double getActuatorPositionRad() {
+  public double getActuatorPositionRad() { // same as position but in radians
     return actuatorInputs.turnPositionRad;
   }
 
@@ -76,7 +98,8 @@ public class Actuator extends SubsystemBase {
         percent * 12); // sets the speed based on a percentage not just voltge
   }
 
-  private void updateSetpoint(double newSetpoint) {
-    actuatorSetpoint = newSetpoint;
+  private void updateSetpoint() {
+    actuatorSetpoint = SmartDashboard.getNumber("actuatorSetpoint", 0.0);
+    actuatorPID.setSetpoint(actuatorSetpoint);
   }
 }
