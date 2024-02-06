faa9836de1281b5a3bc75752d5d3e77e96a0cfe0
alt09 <andreslaratrevino@gmail.com>
2024-02-05 18:51:15 -0700

    make a wrist file without PID for the wrist,creating 2 functions for set the speed of the first and second motor of the wrist

--- src/main/java/frc/robot/Subsystems/wrist/Wrist.java
+++ src/main/java/frc/robot/Subsystems/wrist/Wrist.java
@@ -4,5 +4,39 @@
 
 package frc.robot.Subsystems.wrist;
 
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 /** Add your docs here. */
-public class Wrist {}
+public class Wrist extends SubsystemBase {
+
+    private final WristIO io;
+    private final WristIOInputsAutoLogged wristInputs = new WristIOInputsAutoLogged();
+    
+    public Wrist(wristIONeo io) {
+        System.out.println("[Init] Creating wrist");
+
+        this.io = io;
+    }
+
+    @Override
+    public void periodic(){
+
+        this.updateInputs();
+        Logger.processInputs("wrist", wristInputs);
+    }
+
+    public void updateInputs() {
+        io.updateInputs(wristInputs);
+    }
+
+  /** Sets speed for the first motor of the wrist */
+  public void setFirstWristSpeed(double speed) {
+    io.setFirstWristSpeed(speed);
+  }
+  public void setSecondWristMotorSpeed(double speed) {
+    io.setSecondWristSpeed(speed);
+  }
+}
