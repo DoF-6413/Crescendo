5124269958abc245da74ab20eb6cad5418b41f09
hh3len <89624311+hh3len@users.noreply.github.com>
2024-02-08 17:53:28 -0700

    Working PID in general, also working Shooter code (#36)
    * Inital commit, updated Beta Number, coverted shooter into a profiledPIDSubsystem, added PID constants for the shooter
    
    * Added more Shooter constants, logged Shooter motor acceleration to AdvantageScope
    
    * Changed shooter profiledPIDsubsystem to PIDsubsystem and added more PID constants
    
    * updated shooter constants
    
    * Added feedforward constants and tolerance to the shooter subsystem, added a atSetpoint method to shooter subsystem
    
    * added slotsconfig to ShooterIOTalonFX
    
    * Tried implementing PID in the sim but failed, updated shooter pid constants
    
    * Updated setpoint to 4000 RPM, updated useOutput, removed PID controller from shooterIOSim due to not being needed, changing PID & FF numbers do not seem to affect sim, added comments
    
    * Switched to PID contoller and created tunable PID numbers on the SmartDashboard/Shuffleboard
    
    * Created a sepereate PID controller for each motor, added a setSetpoint method from the PIDController class
    
    * Added comments explaining code, created an AtSetpoint method for the top and bottom motors
    
    * Added feedforward to the the shooter, updated the way the position error is gotten (should still result in the same value), updated the way velocity is gotten
    
    * Removed FF controller, updated the way the voltage is set based on the PID contoller
    
    * Cleaned up code, changed 0 to 0.0
    
    * Changed Calculate PID method
    
    * Minor changes
    
    * changed back t top
    
    * Manual PID
    
    * Cleaned up code, added commments
    
    * Created Custom PID Solution, needs testing READY FOR TESTING
    
    * Updated Beta Number
    
    * fixed minor issues: setPID now updates all PID values, added atSetpoint boolean, cleaned and updated comments, added getPID to return all PID values in an array, attempted to implement tolerance (kept all prior code)
    
    * beta number
    
    * Tried different things for the PID contoller but prob wont use but we should commit bc we can always undo changes�
    
    * Combined the percent speed solution worked on 2/5/24 and the voltage solution so that both can be tested without having to change braches/revert changes, neither seem to work fully
    
    * working pid code for top shooter, wip for bottom
    
    * working pid code ‼‼�
    
    * MADE THE SHOT!!
    
    * with pookie wookie josue jesus, got pid working, added motorRPMdifference, made like 4 shots out of idk how many but not important :)
    
    * Changed method for calculating for voltage in PID contoller (untested), created a sim for the shooter PID by adding PID contollers to the shooterSimIO (sorry liz i didnt know how else to do it T-T)
    
    * IT WOKRS AAAAAA, requires some PID tuning, added back updates from smartdashboard
    
    * Last commit before PR, cleaned up code, updated beta number to reflect how it will look once in Dev
    
    * cleaned code further, added comments, updated beta number for PR
    
    ---------
    
    Co-authored-by: DragomHype <josueraygoza2@gmail.com>
    Co-authored-by: El1zabethS <Bestlemon555@gmail.com>

--- src/main/java/frc/robot/Subsystems/shooter/ShooterIOSim.java
+++ src/main/java/frc/robot/Subsystems/shooter/ShooterIOSim.java
@@ -2,21 +2,26 @@ package frc.robot.Subsystems.shooter;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
-import frc.robot.Constants.*;
+import frc.robot.Constants.RobotStateConstants;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.Utils.PIDController;
 
 public class ShooterIOSim implements ShooterIO {
   // Creating flywheels
   private FlywheelSim topShooterFlywheel =
       new FlywheelSim(
           DCMotor.getFalcon500(1),
-          ShooterConstants.GEAR_RATIO,
+          ShooterConstants.SHOOTER_GEAR_RATIO,
           ShooterConstants.SHOOTER_J_KG_METERS_SQUARED);
 
   private FlywheelSim bottomShooterFlywheel =
       new FlywheelSim(
           DCMotor.getFalcon500(1),
-          ShooterConstants.GEAR_RATIO,
+          ShooterConstants.SHOOTER_GEAR_RATIO,
           ShooterConstants.SHOOTER_J_KG_METERS_SQUARED);
+
+  private PIDController topShooterPID;
+  private PIDController bottomShooterPID;
 
   public ShooterIOSim() {
     System.out.println("[Init] Creating ShooterIOSim");
@@ -24,24 +29,45 @@ public class ShooterIOSim implements ShooterIO {
 
   @Override
   public void updateInputs(ShooterIOInputs inputs) {
+    topShooterPID = new PIDController(1, 0, 0);
+    topShooterPID.setSetpoint(1000);
+    
     // Updates the Shooter motors periodically
     topShooterFlywheel.update(RobotStateConstants.LOOP_PERIODIC_SEC);
     bottomShooterFlywheel.update(RobotStateConstants.LOOP_PERIODIC_SEC);
-
+    
     // Updates logged inputs, RPM and current. Voltage and temp aren't updated because ideally (like
     // in a simulation), they would be constant
     inputs.topShooterMotorRPM = topShooterFlywheel.getAngularVelocityRPM();
     inputs.topShooterCurrentAmps = new double[] {Math.abs(topShooterFlywheel.getCurrentDrawAmps())};
-
     inputs.bottomShooterMotorRPM = bottomShooterFlywheel.getAngularVelocityRPM();
     inputs.bottomShooterCurrentAmps =
-        new double[] {Math.abs(bottomShooterFlywheel.getCurrentDrawAmps())};
-  }
-
+    new double[] {Math.abs(bottomShooterFlywheel.getCurrentDrawAmps())};
+    
+    topShooterFlywheel.setInputVoltage(
+      topShooterPID.calculateForVoltage(inputs.topShooterMotorRPM, 6350));
+    }
+    
+  
+    // TODO: Update below methods to implement PID
   @Override
-  public void setShooterMotorPercentSpeed(double percent) {
-    // sets voltage
+  public void setBothShooterMotorPercentSpeed(double percent) {
+    // Sets the speed based on a percentage of the voltage
     topShooterFlywheel.setInputVoltage(ShooterConstants.APPLIED_VOLTS * percent);
     bottomShooterFlywheel.setInputVoltage(ShooterConstants.APPLIED_VOLTS * percent);
   }
+  
+  @Override
+  public void setBothShooterMotorsVoltage(double volts) {
+    // Sets voltage
+    topShooterFlywheel.setInputVoltage(volts);
+    bottomShooterFlywheel.setInputVoltage(volts);
+  }
+
+
+  @Override
+  public void setTopShooterMotorVoltage(double volts) {}
+
+  @Override
+  public void setBottomShooterMotorVoltage(double volts) {}
 }
