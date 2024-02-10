2554fbfb0082f8ff2c0d0f28629fcb62c4e4c58f
alt09 <andreslaratrevino@gmail.com>
2024-02-10 12:48:06 -0700

    separate both motors of the wrist

--- src/main/java/frc/robot/Subsystems/wrist/WristIO.java
+++ src/main/java/frc/robot/Subsystems/wrist/WristIO.java
@@ -7,25 +7,16 @@ public interface WristIO {
   @AutoLog
   public static class WristIOInputs {
 
-    // the first root of the wrist
-
-    public double firstWristTurnAppliedVolts = 0.0;
-    public double firstWristTurnPositionRad = 0.0;
-    public double firstWristTurnVelocityRadPerSec = 0.0;
-    public double firstWristTurnCurrentAmps = 0.0;
-    public double firstWristTempCelcius = 0.0;
-
-    // the second root of the wrist (the closest to the shooter)
-    public double secondWristTurnAppliedVolts = 0.0;
-    public double secondWristTurnPositionRad = 0.0;
-    public double secondWristTurnVelocityRadPerSec = 0.0;
-    public double secondWristTurnCurrentAmps = 0.0;
-    public double secondWristTempCelcius = 0.0;
+    public double wristTurnAppliedVolts = 0.0;
+    public double wristTurnPositionRad = 0.0;
+    public double wristTurnVelocityRadPerSec = 0.0;
+    public double wristTurnCurrentAmps = 0.0;
+    public double wristTempCelcius = 0.0;
   }
 
   public default void updateInputs(WristIOInputs inputs) {}
 
-  public default void setWristMotorsSpeed(double firstSpeed, double secondSpeed) {
+  public default void setWristMotorSpeed(double peed) {
     /** Sets speed for the first motor(the farest of the shooter) of the wrist */
   }
 }
