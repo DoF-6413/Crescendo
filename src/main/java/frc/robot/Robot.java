425a659d924a662fc198b0dc801316bef758a06b
hh3len <helenjling@gmail.com>
2024-02-14 19:33:21 -0700

    merged pr?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -62,8 +63,7 @@ public class Robot extends LoggedRobot {
     switch (RobotStateConstants.getMode()) {
       case REAL:
         // Running on a real robot, log to a USB stick ("/U/logs")
-        Logger.addDataReceiver(new WPILOGWriter("/Crescendo/Logs")); // The name looks like
-        // "Logs_Year-Month-Day_Hour-Minute-Second"
+        Logger.addDataReceiver(new WPILOGWriter("/Crescendo/Logs")); // The name looks like "Logs_Year-Month-Day_Hour-Minute-Second"
         Logger.addDataReceiver(new NT4Publisher());
         break;
 
