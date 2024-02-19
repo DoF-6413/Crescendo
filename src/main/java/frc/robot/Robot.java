f0295e5b65e69973631d9a3574f6dce2a2708fc8
hh3len <helenjling@gmail.com>
2024-02-18 23:39:27 -0700

    added DriveConstants file, updated PID & FF constants, currently robot drives mostly straight and doesn't turn

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -92,7 +92,7 @@ public class Robot extends LoggedRobot {
     // Beta Numbers (Repository Number, Pushes to Dev, Issue Number, Commit Number, If it Works)
     // (For if it works: 1 = Working, 0 = Works, but not as intended, -1 = Crashes, -2 Doesn't
     // Build)
-    SmartDashboard.putString("Beta Number", "1.14.4.86.1");
+    SmartDashboard.putString("Beta Number", "1.14.4.87.1");
 
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our autonomous chooser on the dashboard.
