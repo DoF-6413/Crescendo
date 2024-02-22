4c69c7e433e00b864050d3e5d2bd59b58bfb09ab
Dragomhype <josueraygoza2@gmail.com>
2024-02-22 16:19:29 -0700

    Inital commit/boilerplate

---
+++ src/main/java/frc/robot/Subsystems/indexer/Indexer.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems.indexer;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Indexer extends SubsystemBase {
+  /** Creates a new indexer. */
+  public Indexer() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/frc/robot/Subsystems/indexer/IndexerIOTalonFX.java
@@ -0,0 +1,5 @@
+package frc.robot.Subsystems.indexer;
+
+public class IndexerIOTalonFX implements IndexerIO {
+    
+}
