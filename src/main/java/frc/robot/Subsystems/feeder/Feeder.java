840cb9b048ce2fe8b6c5de8fc2a5b4a1ccba26dc
Dragomhype <josueraygoza2@gmail.com>
2024-02-22 17:15:00 -0700

    Completed feeder motor contol, including with PID control, UNTESTED

--- src/main/java/frc/robot/Subsystems/feeder/Feeder.java
+++ src/main/java/frc/robot/Subsystems/feeder/Feeder.java
@@ -4,16 +4,19 @@
 
 package frc.robot.Subsystems.feeder;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Subsystems.feeder.FeederIO;
 import frc.robot.Utils.PIDController;
 
 public class Feeder extends SubsystemBase {
   private final FeederIO io;
   private final FeederIOInputsAutoLogged inputs = new FeederIOInputsAutoLogged();
+ 
+  // Creates adjustable PID values on a Shuffleboard tab
   private final ShuffleboardTab feederTab = Shuffleboard.getTab("Feeder");
   private GenericEntry feederkP;
   private GenericEntry feederkI;
@@ -30,14 +33,72 @@ public class Feeder extends SubsystemBase {
     feederPIDController = new PIDController(FeederConstants.KP, FeederConstants.KI, FeederConstants.KD);
     feederPIDController.setSetpoint(setpointRPM);
     feederPIDController.setTolerance(setpointRPM * FeederConstants.TOLERANCE_PERCENT);
+
+     // Puts adjustable PID values and setpoints onto the SmartDashboard
+    feederkP = feederTab.add("feederkP", 0.0).getEntry();
+    feederkI = feederTab.add("feederkI", 0.0).getEntry();
+    feederkD = feederTab.add("feederkD", 0.0).getEntry();
+    feederSetpointSetter = feederTab.add("feederSetpoint", 0.0).getEntry();
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    this.updateInputs();
+    Logger.processInputs("Feeder", inputs);
+
+    setFeederVoltage(
+      feederPIDController.calculateForVoltage(inputs.feederRPM, FeederConstants.MAX_VALUE));
+
+    if (FeederConstants.KP != feederkP.getDouble(0.0)
+        || FeederConstants.KI != feederkI.getDouble(0.0)
+        || FeederConstants.KD != feederkD.getDouble(0.0)){
+      updatePIDController();
+    }
   }
 
+  /** Updates the set of loggable inputs for both Shooter Motors */
   public void updateInputs() {
     io.updateInputs(inputs);
+  }
+
+  /** Updates the PID values to what they are set to on the SmartDashboard */
+  public void updatePIDController() {
+    FeederConstants.KP = feederkP.getDouble(0.0);
+    FeederConstants.KP = feederkP.getDouble(0.0);
+    FeederConstants.KP = feederkP.getDouble(0.0);
+    feederPIDController.setPID(FeederConstants.KP, FeederConstants.KI, FeederConstants.KD);
+  }
+
+  /** Updates the setpoint to what is typed on the SmartDashboard */
+  public void updateSetpoint() {
+    setpointRPM = feederSetpointSetter.getDouble(0.0);
+    feederPIDController.setSetpoint(setpointRPM);
+  }
+
+  /**
+   * Sets the voltage of the Feeder motor
+   * 
+   * @param volts [-12 to 12]
+   */
+  public void setFeederVoltage(double volts) {
+    io.setFeederVoltage(volts);
+  }
+
+  /**
+   * Sets the speed of the Feeder motor based on a percent of its maximum speed
+   * 
+   * @param percent [-1 to 1]
+   */
+  public void setFeederPercentSpeed(double percent) {
+    io.setFeederPercentSpeed(percent);
+  }
+
+  /**
+   * Sets the Feeder motor to brake mode
+   * 
+   * @param enable
+   */
+  public void setFeederBrakeMode(boolean enable) {
+    io.setFeederBrakeMode(enable);
   }
 }
