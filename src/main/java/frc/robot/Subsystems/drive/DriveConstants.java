24a0d4730654077d9fa5166a60bc2c3e830aa058
hh3len <helenjling@gmail.com>
2024-03-23 08:11:37 -0700

    Update wheel offsets

--- src/main/java/frc/robot/Subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/Subsystems/drive/DriveConstants.java
@@ -166,10 +166,10 @@ public final class DriveConstants {
   }
 
   public static enum L3_ABSOLUTE_ENCODER_OFFSET_RAD {
-    FRONT_RIGHT(0.63), // Module 0
-    FRONT_LEFT(2.363), // Module 1
-    BACK_LEFT(0.09), // Module 2
-    BACK_RIGHT(-2.6185); // Module 3
+    FRONT_RIGHT(0.606), // Module 0
+    FRONT_LEFT(2.441), // Module 1
+    BACK_LEFT(-0.331), // Module 2
+    BACK_RIGHT(-2.6055); // Module 3
 
     public final double OFFSET;
 
