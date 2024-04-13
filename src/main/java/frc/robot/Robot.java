7ecd56645376d9977aaf8d61a99a73175ce2b4f1
Dragomhype <Josueraygoza2@gmail.com>
2024-04-12 21:50:03 -0700

    4P midfield 2.0 ready to test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -90,7 +90,7 @@ public class Robot extends LoggedRobot {
     // Beta Numbers (Repository Number, Pushes to Dev, Issue Number, Commit Number, If it Works)
     // (For if it works: 1 = Working, 0 = Works, but not as intended, -1 = Crashes, -2 Doesn't
     // Build)
-    SmartDashboard.putString("Beta Number", "1.40.69.98.0");
+    SmartDashboard.putString("Beta Number", "1.40.69.146.0");
 
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our autonomous chooser on the dashboard.
