cefb231f3359c3056a489938e63387e069fe6657
Josue Raygoza <Josue.Raygoza@sisepuedefoundation.org>
2024-09-25 18:36:24 -0700

    PR Ready

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -88,7 +88,7 @@ public class Robot extends LoggedRobot {
     // Beta Numbers (Repository Number, Pushes to Dev, Issue Number, Commit Number, If it Works)
     // (For if it works: 1 = Working, 0 = Works, but not as intended, -1 = Crashes, -2 Doesn't
     // Build)
-    SmartDashboard.putString("Beta Number", "1.69.119.69.0");
+    SmartDashboard.putString("Beta Number", "1.70.0.70.1");
     SmartDashboard.putString("Last Deployed at: ", BuildConstants.BUILD_DATE);
 
     // Instantiate our RobotContainer. This will perform all our button bindings,
