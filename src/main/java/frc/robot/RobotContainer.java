84d43d0212bb68983b070c5298a0abadeff6e789
Josue Raygoza <137247197+DragomHype@users.noreply.github.com>
2025-01-07 18:04:58 -0700

    Feat#133 pigeon (#135)
    * Switched from NavX to Pigeon
    
    * Pigeon now fully functioning. Anlges from gyro readings were being converted to radians twice, corrected Pigeon CAN ID, removed USB logging, commented out an auto that caused crashing
    
    * Version number
    
    ---------
    
    Co-authored-by: Josue Raygoza <Josue.Raygoza@sisepuedefoundation.org>
    Co-authored-by: Helen Ling <Helen.Ling@sisepuedefoundation.org>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -100,7 +100,7 @@ public class RobotContainer {
     switch (RobotStateConstants.getMode()) {
       case REAL:
         // Real robot, instantiates hardware IO implementations
-        m_gyroSubsystem = new Gyro(new GyroIONavX());
+        m_gyroSubsystem = new Gyro(new GyroIOPigeon2());
         m_driveSubsystem =
             new Drive(
                 new ModuleIOSparkMaxTalonFX(0),
@@ -393,9 +393,9 @@ public class RobotContainer {
     autoChooser.addOption(
         "4 Piece Center (Podium) (C2, C3, M3)",
         new PathPlannerAuto("4P SubCenter-C2-Sub-C3-Sub-M3 (V)"));
-    autoChooser.addOption(
-        "4 Piece Center (Podium) (C2, C3, M4)",
-        new PathPlannerAuto("4P SubCenter-C2-Sub-C3-Sub-M4 (V)"));
+    // autoChooser.addOption(
+    //     "4 Piece Center (Podium) (C2, C3, M4)",
+    //     new PathPlannerAuto("4P SubCenter-C2-Sub-C3-Sub-M4 (V)"));
 
     // Adds an "auto" tab on ShuffleBoard
     Shuffleboard.getTab("Auto").add(autoChooser.getSendableChooser());
