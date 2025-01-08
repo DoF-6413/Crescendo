a824aec24105f3b4ba9ad11b8eb6af47217aebeb
alt09 <andreslaratrevino@gmail.com>
2025-01-07 19:09:44 -0700

    update all libraries and update the code to 2024 WIP

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -59,7 +59,7 @@ public class Robot extends LoggedRobot {
     switch (RobotStateConstants.getMode()) {
       case REAL:
         // Running on a real robot, log to a USB stick
-        Logger.addDataReceiver(new WPILOGWriter("/media/sda1")); // The name looks like
+        // Logger.addDataReceiver(new WPILOGWriter("/media/sda1")); // The name looks like
         // "Logs_Year-Month-Day_Hour-Minute-Second"
         Logger.addDataReceiver(new NT4Publisher());
         break;
@@ -88,7 +88,7 @@ public class Robot extends LoggedRobot {
     // Beta Numbers (Repository Number, Pushes to Dev, Issue Number, Commit Number, If it Works)
     // (For if it works: 1 = Working, 0 = Works, but not as intended, -1 = Crashes, -2 Doesn't
     // Build)
-    SmartDashboard.putString("Beta Number", "1.76.0.58.1");
+    SmartDashboard.putString("Beta Number", "1.77.134.78.-2");
     SmartDashboard.putString("Last Deployed at: ", BuildConstants.BUILD_DATE);
 
     // Instantiate our RobotContainer. This will perform all our button bindings,
